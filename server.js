2688643c94c9b0d2564c997fe380d9a946e4d9b0
ZP <zairaperez01@gmail.com>
2022-04-11 21:22:40 -0500

    fixed mongoose connection

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+mongoose.set('debug', true);
+app.listen(PORT, () =>
+  console.log(`App running on port ${PORT}!`));
