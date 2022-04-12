01f17e56e965f7066c520764b06d38557666cf85
ZP <zairaperez01@gmail.com>
2022-04-11 21:18:19 -0500

    deploying

--- server.js
+++ server.js
@@ -16,10 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Keeping-Up',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
